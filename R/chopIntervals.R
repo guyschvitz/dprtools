b20ad75a389b6631cb9fa0584a909cc10d9e9edf
guyschvitz <guy.schvitz@gess.ethz.ch>
2021-06-28 16:06:49 +0800

    fixed if_else bug in chopIntervals function

--- R/chopIntervals.R
+++ R/chopIntervals.R
@@ -12,19 +12,30 @@
 ##' @export
 
 chopIntervals <- function(data, id.vars, start.col, end.col){
+
+  ## Function to convert numeric or string dates
+  ## to date format (ifelse converts dates into numeric)
+  dateConv <- function(x){
+    if(inherits(x, "Date")){
+      return(as.Date(x))
+    } else {
+      return(x)
+    }}
+
+  ## Main code
   data %>%
     dplyr::select(c(all_of(id.vars), {{start.col}}, {{end.col}})) %>%
     pivot_longer(names_to = "var", values_to = "val", c(start.col, end.col)) %>%
-    mutate(end = if_else(var==end.col, T,
-                         if_else(var == start.col, F, NA))) %>%
+    mutate(end = ifelse(var==end.col, T,
+                         ifelse(var == start.col, F, NA))) %>%
     group_by_at(id.vars) %>%
     arrange_at(c(id.vars, "val")) %>%
     distinct() %>%
     mutate(end_next = lead(end),
            val_next = lead(val)) %>%
     filter(!is.na(end_next)) %>%
-    mutate(start = if_else(!end, val, val + 1),
-           end = if_else(!end_next, val_next - 1, val_next)) %>%
+    mutate(start = ifelse(!end, val, val + 1),
+           end = ifelse(!end_next, val_next - 1, val_next)) %>%
     filter(end >= start) %>%
     dplyr::select(all_of(id.vars), start, end)
 }
